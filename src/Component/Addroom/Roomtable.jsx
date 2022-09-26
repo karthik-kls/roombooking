7eae5f24171cbe4c3f134b64a4c5666da4e8441d
Your Name <you@example.com>
2022-09-26 15:58:26 +0530

    added edit button

--- src/Component/Addroom/Roomtable.jsx
+++ src/Component/Addroom/Roomtable.jsx
@@ -2,19 +2,29 @@ import React from 'react'
 import '../Addroom/Roomtable.css'
 import Datatable from './Datatable'
 
-const Roomtable = ({data}) => {
+const Roomtable = ({data,setEditbtn,setisEditing})  => {
   return (
    <div className='roomtabletitle'>
+     <div className="mainroomtable">
     <div className='room3'>
     <div className='roomtext'>Room Number</div>
     <div className='roomtext'>Adult Capacity</div>
-    <div className='roomtext'>Children Capacity</div>
+    <div className='roomtext'>ChildrenCapacity</div>
     <div className='roomtext'>Price</div>
+    
+  
+    <div className='roomtext'>Edit</div>
+ 
+  
+    <div className='roomtext'>Delete</div>
+  
+    </div>
+
     </div>
     <div className='list'>
     {data.map((data,index)=>{
         return(
-            <Datatable data={data} key={index} />
+          <Datatable data={data} key={index} setEditbtn={setEditbtn} setisEditing={setisEditing}/>
 
         )
     })}
