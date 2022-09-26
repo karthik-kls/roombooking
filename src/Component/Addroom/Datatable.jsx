6d15d609b78589c3dc3ba0d684dee50dce8f810a
Your Name <you@example.com>
2022-09-26 15:57:58 +0530

    added edit button

--- src/Component/Addroom/Datatable.jsx
+++ src/Component/Addroom/Datatable.jsx
@@ -1,15 +1,34 @@
-import React from 'react'
+import React from "react";
 
-const Datatable = ({data}) => {
+const Datatable = ({ data, setEditbtn, setisEditing }) => {
   return (
-    <div className='valuetableroom'>
-    <div className='orange'>{data.roomnumber}</div>
-    <div>{data.adultcapacity}</div>
-    <div>{data.childrencapacity}</div>
-    <div className='red'>{data.price}</div>
+    <div className="valuetableroom">
+      <div className="tablevalue">
+        <div className="orange">{data.roomNumber}</div>
+        <div>{data.adultCapacity}</div>
+        <div>{data.childCapacity}</div>
+        <div className="red">{data.price}</div>
+
+        <div className="threebtndiv">
+          <div
+            className="threebtn"
+            onClick={() => {
+              setEditbtn(true);
+              setisEditing(true);
+            }}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 512 512"
+              height="17px"
+            >
+              <path d="M471.6 21.7c-21.9-21.9-57.3-21.9-79.2 0L362.3 51.7l97.9 97.9 30.1-30.1c21.9-21.9 21.9-57.3 0-79.2L471.6 21.7zm-299.2 220c-6.1 6.1-10.8 13.6-13.5 21.9l-29.6 88.8c-2.9 8.6-.6 18.1 5.8 24.6s15.9 8.7 24.6 5.8l88.8-29.6c8.2-2.8 15.7-7.4 21.9-13.5L437.7 172.3 339.7 74.3 172.4 241.7zM96 64C43 64 0 107 0 160V416c0 53 43 96 96 96H352c53 0 96-43 96-96V320c0-17.7-14.3-32-32-32s-32 14.3-32 32v96c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V160c0-17.7 14.3-32 32-32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H96z" />
+            </svg>{" "}
+          </div>
+        </div>
+      </div>
     </div>
+  );
+};
 
-  )
-}
-
-export default Datatable
+export default Datatable;
